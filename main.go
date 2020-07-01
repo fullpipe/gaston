5c9975c29754768c81f0f0649e4b29aabebf07ac
Eugene Bravov <eugene.bravov@gmail.com>
2020-07-01 13:13:50 +0300

    Get methods from json files

--- main.go
+++ main.go
@@ -2,66 +2,45 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"path/filepath"
 	"time"
 
+	"github.com/fullpipe/gaston/pkg/config"
 	"github.com/fullpipe/gaston/pkg/converter"
 	"github.com/fullpipe/gaston/pkg/remote"
 	"github.com/fullpipe/gaston/pkg/server"
 )
 
 func main() {
+	files, err := filepath.Glob("methods/*.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	collection := remote.NewMethodCollection()
+	for _, f := range files {
+		temp, _ := ioutil.ReadFile(f)
+		c, err := config.JsonStringToCollection(string(temp))
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		collection = collection.Merge(c)
+	}
+
+	fmt.Println(collection)
+
 	// TODO: build collection from config file
-	collection := remote.MethodCollection{
+	c2 := remote.MethodCollection{
 		Methods: []remote.Method{
 			remote.Method{
-				Host:   "http://localhost:9091/rpc",
-				Name:   "test1",
-				Rename: "s1_test",
-				Roles:  []string{"asd", "ROLE_USER"},
-				ParamConverters: []converter.Converter{
-					&converter.Rename{
-						From: "email_input",
-						To:   "email",
-					},
-				},
-				ResultConverters: []converter.Converter{
-					&converter.Rename{
-						From: "s1_test",
-						To:   "s1_success",
-					},
-				},
-			},
-			remote.Method{
-				Host:   "http://localhost:9091/rpc",
-				Name:   "test2",
-				Rename: "s1_test2",
-				Roles:  []string{"asd", "ROLE_USER"},
-				ParamConverters: []converter.Converter{
-					&converter.Rename{
-						From: "email_input",
-						To:   "email",
-					},
-				},
-			},
-			remote.Method{
-				Host:   "http://localhost:9092/rpc",
-				Name:   "test3",
-				Rename: "s2_test",
-				Roles:  []string{"asd", "ROLE_USER"},
-				ParamConverters: []converter.Converter{
-					&converter.Rename{
-						From: "email_input",
-						To:   "email",
-					},
-				},
-			},
-			remote.Method{
-				Host:   "http://localhost:9092/rpc",
-				Name:   "test4",
-				Rename: "s2_test2",
-				Roles:  []string{"asd", "ROLE_USER"},
+				Host:       "http://localhost:9092/rpc",
+				Name:       "test5",
+				RemoteName: "s2_test2",
+				Roles:      []string{"asd", "ROLE_USER"},
 				ParamConverters: []converter.Converter{
 					&converter.Rename{
 						From: "email_input",
@@ -71,6 +50,8 @@ func main() {
 			},
 		},
 	}
+
+	collection = collection.Merge(c2)
 
 	// get config from config file?
 	tr := &http.Transport{
